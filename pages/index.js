2ef3f58e9d33d753db0ade07fa1e3b638c3120fb
Hassan Marzooq <marzooq703@gmail.com>
2022-09-13 14:24:45 +0530

    Added Home Screen navigation to create-user module

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 const Home = () => {
   return (
     <>
-    Email:
-    <input id="email" />
+      <h1>CRM Playground</h1>
+      <Link href="/create-user">
+        <button>Create User</button>
+      </Link>
     </>
-  )
-}
+  );
+};
 export default Home;
